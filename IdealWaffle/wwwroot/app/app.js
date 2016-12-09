a9a6d4617bcdef84d7445f0052dcc6fc83677e9e
Dia6lo <Dgamjm@gmail.com>
2016-12-09 22:05:02 +1000

    Test

--- IdealWaffle/wwwroot/app/app.js
+++ IdealWaffle/wwwroot/app/app.js
@@ -9,7 +9,21 @@ class Game extends Application {
         root.pivot = Vector2.half;
         root.position = this.renderer.size.divide(2);
         this.root = root;
+        this.getImagePixels();
         super.update(delta);
+    }
+    getImagePixels() {
+        const img = new Image();
+        img.onload = ev => {
+            var canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var context = canvas.getContext("2d");
+            context.drawImage(img, 0, 0, img.width, img.height);
+            var imageData = context.getImageData(0, 0, img.width, img.height);
+            var a = 1;
+        };
+        img.src = "assets/Color_Test.png";
     }
 }
 var game;
