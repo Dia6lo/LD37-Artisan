3ad03c3a42bddea573e783cea91b31159156bba8
Dia6lo <Dgamjm@gmail.com>
2016-12-10 01:28:26 +1000

    Add cool motions.

--- IdealWaffle/app/Game.ts
+++ IdealWaffle/app/Game.ts
@@ -1,18 +1,62 @@
 ﻿class Game extends Application {
-
     constructor() {
         super();
         this.renderer.backgroundColor = Color.skyblue;
-    }
-
-    update(delta: number): void {
         const root = Sprite.fromImage("assets/Logo.jpg");
         root.size = new Vector2(385, 131);
         root.pivot = Vector2.half;
-        root.position = this.renderer.size.divide(2);
+        root.tasks.add(this.moveTask(root));
+        root.tasks.add(this.rotateTask(root));
         this.root = root;
-        this.getImagePixels();
+    }
+
+    update(delta: number): void {
         super.update(delta);
+    }
+
+    private *moveTask(sprite: Sprite) {
+        const center = this.renderer.size.divide(2);
+        const to = center.multiply(new Vector2(1.5, 1));
+        const from = to.multiply(new Vector2(0.5, 1));
+        sprite.position = from;
+        for (let t of this.easeInOutBackMotion(2, from.x, to.x)) {
+            sprite.x = t;
+            yield Wait.frame();
+        }
+    }
+
+    private *rotateTask(sprite: Sprite) {
+        for (let t of Task.linearMotion(0.4, 0, -15)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+        for (let t of Task.linearMotion(0.4, -15, 0)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+        yield Wait.seconds(0.3);
+        for (let t of Task.linearMotion(0.4, 0, 15)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+        for (let t of Task.linearMotion(0.4, 15, 0)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+    }
+
+    private *easeInOutBackMotion(timePeriod: number, from: number, to: number) {
+        for (let t of Task.motion(timePeriod, from, to, fraction => this.easeInOutBack(fraction))) {
+            yield t;
+        }
+    }
+
+    private easeInOutBack(x: number) {
+        const c1 = 1.70158;
+        const c2 = c1 * 1.525;
+        return x < 0.5 ?
+            (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 :
+            (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
     }
 
     getImagePixels() {
--- IdealWaffle/wwwroot/app/app.js
+++ IdealWaffle/wwwroot/app/app.js
@@ -2,15 +2,56 @@ class Game extends Application {
     constructor() {
         super();
         this.renderer.backgroundColor = Color.skyblue;
-    }
-    update(delta) {
         const root = Sprite.fromImage("assets/Logo.jpg");
         root.size = new Vector2(385, 131);
         root.pivot = Vector2.half;
-        root.position = this.renderer.size.divide(2);
+        root.tasks.add(this.moveTask(root));
+        root.tasks.add(this.rotateTask(root));
         this.root = root;
-        this.getImagePixels();
+    }
+    update(delta) {
         super.update(delta);
+    }
+    *moveTask(sprite) {
+        const center = this.renderer.size.divide(2);
+        const to = center.multiply(new Vector2(1.5, 1));
+        const from = to.multiply(new Vector2(0.5, 1));
+        sprite.position = from;
+        for (let t of this.easeInOutBackMotion(2, from.x, to.x)) {
+            sprite.x = t;
+            yield Wait.frame();
+        }
+    }
+    *rotateTask(sprite) {
+        for (let t of Task.linearMotion(0.4, 0, -15)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+        for (let t of Task.linearMotion(0.4, -15, 0)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+        yield Wait.seconds(0.3);
+        for (let t of Task.linearMotion(0.4, 0, 15)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+        for (let t of Task.linearMotion(0.4, 15, 0)) {
+            sprite.rotation = t;
+            yield Wait.frame();
+        }
+    }
+    *easeInOutBackMotion(timePeriod, from, to) {
+        for (let t of Task.motion(timePeriod, from, to, fraction => this.easeInOutBack(fraction))) {
+            yield t;
+        }
+    }
+    easeInOutBack(x) {
+        const c1 = 1.70158;
+        const c2 = c1 * 1.525;
+        return x < 0.5 ?
+            (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 :
+            (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
     }
     getImagePixels() {
         const img = new Image();
